ec69c5669850c7151d2f4558e89d6dd141629ba4
Blues <i@iblues.name>
2020-03-10 11:35:33 +0800

    改登录为邮箱

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -33,7 +33,7 @@ const actions = {
   login({ commit }, userInfo) {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
-      login({ phone: username.trim(), password: password }).then(response => {
+      login({ email: username.trim(), password: password }).then(response => {
         const { data } = response
         console.log(data.token)
         commit('SET_TOKEN', data.token)
