7776e3f63ca377b72aef236d9891e4019229ea65
Blues <i@iblues.name>
2020-06-11 21:21:36 +0800

    语言包

--- src/main.js
+++ src/main.js
@@ -47,8 +47,8 @@ import larfree from '../../vue-larfree-core/src/index.js'
 Vue.use(larfree, { host: process.env.VUE_APP_BASE_API })
 
 Vue.use(Element, {
-  size: Cookies.get('size') || 'medium', // set element-ui default size
-  locale: enLang // 如果使用中文，无需设置，请删除
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+  // locale: enLang // 如果使用中文，无需设置，请删除
 })
 
 // register global utility filters
