1d4db4d60799287de822d81a840561dcfba68780
Blues <i@iblues.name>
2020-07-01 18:16:02 +0800

    配置列表相关优化. 第一版本完成

--- src/larfree/components/config/index.vue
+++ src/larfree/components/config/index.vue
@@ -4,17 +4,18 @@
     <ui-plane>
       <h2>配置管理中心</h2>
       <el-input
+        v-model="search"
         style="max-width: 350px"
         placeholder="请输入内容"
       >
         <i slot="prefix" class="el-input__icon el-icon-search" />
         <el-button slot="append" icon="el-icon-search">搜索</el-button>
       </el-input>
-      <el-button  icon="el-icon-refresh" @click="getList">刷新</el-button>
+      <el-button icon="el-icon-refresh" @click="getList">刷新</el-button>
 
       <el-row v-loading="loading" :gutter="20">
 
-        <el-col v-for="config in configList" :key="config.key" :xs="24" :sm="12" :md="8" :lg="6">
+        <el-col v-for="config in configs" :key="config.key" :xs="24" :sm="12" :md="8" :lg="6">
           <el-card class="box-card">
             <div slot="header" class="clearfix">
               <span>{{ config.title || '缺失标题' }}</span>
@@ -46,7 +47,8 @@ export default {
   data() {
     return {
       configList: [],
-      loading: false
+      loading: false,
+      search: ''
     }
   },
   created() {
@@ -57,6 +59,31 @@ export default {
       getPipe: 'larfree/getPipe',
       getRefreshEvents: 'larfree/getRefreshEvents'
     }),
+    // 搜索过滤
+    configs() {
+      if (!this.search) {
+        console.log('没有搜索', this.configList)
+        return this.configList
+      }
+      const configs = []
+      this.configList.forEach((data) => {
+        const searchDetail = {}
+        for (const detailKey in data.detail) {
+          // 有搜索到结果
+          if (data.detail[detailKey].name.indexOf(this.search) > -1) {
+            searchDetail[detailKey] = data.detail[detailKey]
+          }
+        }
+
+        // 不为空
+        if ((JSON.stringify(searchDetail) !== '{}')) {
+          const cloneObj = JSON.parse(JSON.stringify(data))// clone
+          cloneObj.detail = searchDetail
+          configs.push(cloneObj)
+        }
+      })
+      return configs
+    },
     dataRefreshEvents() {
       return this.getRefreshEvents('system.config')
     }
