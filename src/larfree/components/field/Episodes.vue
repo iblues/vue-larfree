0a27634ebf16dc21d18421d0e4d46e559f1b1bdf
Blues <i@iblues.name>
2020-07-01 22:02:07 +0800

    调整文字

--- src/larfree/components/field/Episodes.vue
+++ src/larfree/components/field/Episodes.vue
@@ -9,7 +9,7 @@
     >
       <i class="el-icon-upload" />
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+      <div slot="tip" class="el-upload__tip">只能上传mp3/m4a文件，且不超过50mb</div>
     </el-upload>
 
     <h3>编辑排序区域</h3>
